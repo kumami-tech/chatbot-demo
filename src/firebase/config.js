97b9794ca4143cbd8d1b611b1104d56a08fe5b61
kumami-tech <sk924731@gmail.com>
2020-10-15 18:09:36 +0900

    Change dataset to JSON

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyATLtqWS3IMK68_DtKXnA9UfpsgEtCgUgc",
+  authDomain: "chatbot-demo-6c30a.firebaseapp.com",
+  databaseURL: "https://chatbot-demo-6c30a.firebaseio.com",
+  projectId: "chatbot-demo-6c30a",
+  storageBucket: "chatbot-demo-6c30a.appspot.com",
+  messagingSenderId: "759355145415",
+  appId: "1:759355145415:web:156d2c5d405d472994d2e2",
+  measurementId: "G-H2G0GFH8BV"
+};
+
+export default firebaseConfig
